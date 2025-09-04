80f99b5c73547e2bdb487dd1e027cbc547b05ad4
Kndn15 <142054093+Kndn15@users.noreply.github.com>
2025-09-05 00:21:16 +0530

    Update SecurityConfig.java

--- backend/onlinepharma_backend/src/main/java/com/team4/onlinepharma_backend/security/SecurityConfig.java
+++ backend/onlinepharma_backend/src/main/java/com/team4/onlinepharma_backend/security/SecurityConfig.java
@@ -108,7 +108,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); 
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+                                        "https://onlinepharma-frontend.onrender.com")); 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
