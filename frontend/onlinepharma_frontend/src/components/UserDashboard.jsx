6f9ae98bc37df21093963510ad58b3a39bcfe181
Kndn15 <142054093+Kndn15@users.noreply.github.com>
2025-09-05 00:05:09 +0530

    Update UserDashboard.jsx

--- frontend/onlinepharma_frontend/src/components/UserDashboard.jsx
+++ frontend/onlinepharma_frontend/src/components/UserDashboard.jsx
@@ -14,7 +14,7 @@ const UserDashboard = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:8080/api/drugs")
+      .get("https://onlinepharma-backend.onrender.com/api/drugs")
       .then((response) => {
         setDrugs(response.data);
       })
